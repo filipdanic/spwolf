b51698ef778738f39f756ec3dfa399226a4c62cf
Filip Danić <filip@spicefactory.co>
2018-03-06 14:28:22 +0100

    fix bug in validation logic

--- src/SpForm.js
+++ src/SpForm.js
@@ -84,9 +84,9 @@ export const FormHoC = ({ componentMap, wrappers }) => {
         if (i === elements.length - 1) {
           const { diff } = this.getFormState();
           if (Object.keys(diff).length > 0) {
-            this.updateCanSubmitForm(false);
-          } else {
             this.updateCanSubmitForm(true);
+          } else {
+            this.updateCanSubmitForm(false);
           }
         }
       }
